5d8851ed313f6d1a34671d81ff4de6fe7e648b47
刘宏军 <1244795637@qq.co>
2019-01-26 15:24:40 +0800

    购物车框架

--- WX.API/WX.API.C2C/App_Start/AutofacConfig.cs
+++ WX.API/WX.API.C2C/App_Start/AutofacConfig.cs
@@ -10,7 +10,8 @@ using System.Web.Http;
 using Autofac.Integration.Mvc;
 using WX.API.IRepository.Near;
 using WX.API.Repository.Near;
-
+using WX.API.IRepository.Car;
+using WX.API.Repository.Car;
 
 namespace WX.API.C2C
 {
@@ -41,7 +42,7 @@ namespace WX.API.C2C
         public static void SetupResolveRules(ContainerBuilder container)
         {
             container.RegisterType<AdvertisingInfoRepository>().As<IAdvertisingInfoRepository>();//前面是Repository里面的方法 后面写他所继承的借口
-
+            container.RegisterType<ShopCarRepository>().As<IShopCarRepoitory>();
 
         }
     }

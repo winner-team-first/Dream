20305551d3bf1be15c3994becc4f9cef63c671b9
刘宏军 <1244795637@qq.co>
2019-01-26 16:23:12 +0800

    123

---
+++ WX.API/WX.API.C2C/Controllers/ShopCarController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WX.API.MODEL.Car;
+using WX.API.IRepository.Car;
+
+namespace WX.API.C2C.Controllers
+{
+    public class ShopCarController : ApiController
+    {
+        public IShopCarRepoitory ShopCarRepoitory { get; set; }
+
+        [HttpGet]
+        [ActionName("Show")]
+        public List<ShopCar> Show()
+        {
+            return ShopCarRepoitory.Show(); 
+        }
+    }
+}
--- WX.API/WX.API.Repository/Car/ShopCarRepository.cs
+++ WX.API/WX.API.Repository/Car/ShopCarRepository.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using WX.API.IRepository.Car;
 using WX.API.MODEL.Car;
+using System.Configuration;
 
 namespace WX.API.Repository.Car
 {
     public class ShopCarRepository : IShopCarRepoitory
     {
+        string str = ConfigurationManager.ConnectionStrings["ConnectionLhj"].ConnectionString;
+
         public List<ShopCar> Show()
         {
             throw new NotImplementedException();

963755b47f663bc721991ebc57088c393510cdc7
刘嘉博 <jiabo_12138@163.com>
2019-01-27 11:01:51 +0800

    0127

---
+++ WX.API/WX.API.C2C/Controllers/AddressController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Web;
+using WX.API.MODEL.Address;
+using WX.API.IRepository.Address;
+using WX.API.Repository.Address;
+
+namespace WX.API.C2C.Controllers
+{
+    public class AddressController : ApiController
+    {
+        public IAddressRepository IAddr { get; set; }
+
+        /// <summary>
+        /// 收货地址显示
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<MODEL.Address.Address> GetList()
+        {
+            List<MODEL.Address.Address> list = IAddr.GetList();
+            return list;
+        }
+    }
+}
---
+++ WX.API/WX.API.Repository/Address/AddressRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WX.API.IRepository.Address;
+using WX.API.MODEL.Address;
+using Dapper;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace WX.API.Repository.Address
+{
+    public class AddressRepository : IAddressRepository
+    {
+        /// <summary>
+        /// mysql用户刘嘉博的连接字符串
+        /// </summary>
+        string str = ConfigurationManager.ConnectionStrings["ConnectionLjb"].ConnectionString;
+
+        /// <summary>
+        /// 地址信息显示 
+        /// </summary>
+        /// <returns></returns>
+        public List<MODEL.Address.Address> GetList()
+        {
+            using (MySqlConnection conn = new MySqlConnection(str))
+            {
+                conn.Open();
+                List<MODEL.Address.Address> list = conn.Query<MODEL.Address.Address>("select * from Address").ToList();
+                return list;
+            }
+        }
+    }
+}

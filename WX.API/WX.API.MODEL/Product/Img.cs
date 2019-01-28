aeecf1aa98dfff5a36381dc3d32e36408c8a9e54
guozhaoxin2207 <46185963+guozhaoxin2207@users.noreply.github.com>
2019-01-28 12:00:41 +0800

    okik

---
+++ WX.API/WX.API.MODEL/Product/Img.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WX.API.MODEL.Product
+{
+    public  class Img
+    {
+        public int ID { get; set; }
+        public int ProductID { get; set; }
+        public string ProductImage { get; set; }
+    }
+}
